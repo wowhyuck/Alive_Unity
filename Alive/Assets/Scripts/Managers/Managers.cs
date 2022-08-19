0a3439cfb8c0daf4365c5bb6be4192341ac30842
Joohyuck <joohyuck.seo@gmail.com>
2022-08-19 19:40:16 +0900

    매니저 만들기

---
+++ Alive/Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    void Start()
+    {
+        // 초기화
+
+
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
