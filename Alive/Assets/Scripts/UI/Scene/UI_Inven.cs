df1958961082c7cdf0bd3a11a52fc7e216af82aa
Joohyuck <joohyuck.seo@gmail.com>
2022-08-24 06:04:46 +0900

    UI
    : UI 리소스, UI Manager와 기능 추가 및 테스트

---
+++ Alive/Assets/Scripts/UI/Scene/UI_Inven.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Inven : UI_Scene
+{
+    enum GameObjects
+    {
+        UI_Inven_Background
+    }
+
+    void Start()
+    {
+        Init();
+    }
+
+    public override void Init()
+    {
+        base.Init();
+
+        Bind<GameObject>(typeof(GameObjects));
+
+        GameObject background = Get<GameObject>((int)GameObjects.UI_Inven_Background);
+        foreach (Transform child in background.transform)
+            Managers.Resource.Destroy(child.gameObject);
+
+        // 실제 인벤토리 정보를 참고해서
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject item = Managers.UI.MakeSubItem<UI_Inven_Item>(background.transform).gameObject;
+            UI_Inven_Item invenItem = item.GetOrAddComponent<UI_Inven_Item>();
+            invenItem.SetKey($"{i}");
+        }
+    }
+}
---
+++ Alive/Assets/Scripts/UI/SubItem/UI_Inven_Item.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Inven_Item : UI_Base
+{
+    enum GameObjects
+    {
+        Icon,
+        Key,
+        Number,
+    }
+
+    string _key;
+
+    void Start()
+    {
+        Init();   
+    }
+
+    public override void Init()
+    {
+        Bind<GameObject>(typeof(GameObjects));
+
+        Get<GameObject>((int)GameObjects.Key).GetComponent<Text>().text = _key;
+
+        Get<GameObject>((int)GameObjects.Icon).BindEvent((PointerEventData) => { Debug.Log($"아이템 클릭!"); });
+    }
+
+    public void SetKey(string key)
+    {
+        _key = key;
+    }
+    
+}
