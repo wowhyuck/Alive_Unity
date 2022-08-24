3f12c9f18d7ab9d8cf58f877e450c4682b6e67b3
Joohyuck <joohyuck.seo@gmail.com>
2022-08-25 03:16:21 +0900

    유니티짱 Sound 리소스 이동

--- Alive/Assets/Scripts/Controllers/PlayerController.cs
+++ Alive/Assets/Scripts/Controllers/PlayerController.cs
@@ -24,7 +24,6 @@ public class PlayerController : MonoBehaviour
         Managers.Input.MouseAction -= OnMouseClicked;
         Managers.Input.MouseAction += OnMouseClicked;
 
-
     }
 
     void UpdateDie()
