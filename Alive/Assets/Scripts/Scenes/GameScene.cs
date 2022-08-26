2d684117265d83842c33ca79f9ce68953e9c1eac
Joohyuck <joohyuck.seo@gmail.com>
2022-08-26 09:41:31 +0900

    Data Manager 만들기
    : Data Manager 만딜기, json 파일 활용

--- Alive/Assets/Scripts/Scenes/GameScene.cs
+++ Alive/Assets/Scripts/Scenes/GameScene.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class GameScene : BaseScene
 {
-    Coroutine co;
-
     protected override void Init()
     {
         base.Init();
@@ -14,36 +12,11 @@ public class GameScene : BaseScene
 
         Managers.UI.ShowSceneUI<UI_Inven>();
 
-        co = StartCoroutine("CoExplodeAfterSeconds", 4.0f);
-
-        StartCoroutine("CoStopExplode", 2.0f);
+        Dictionary<int, Stat> dict = Managers.Data.StatDict;
     }
-
-    IEnumerator CoStopExplode(float seconds)
-    {
-        Debug.Log("Stop Enter");
-        yield return new WaitForSeconds(seconds);
-        Debug.Log("Stop Exit");
-
-        if (co != null)
-        {
-            StopCoroutine(co);
-            co = null;
-        }
-    }
-
-    IEnumerator CoExplodeAfterSeconds(float seconds)
-    {
-        Debug.Log("Explode Enter");
-        yield return new WaitForSeconds(seconds);
-        Debug.Log("Explode Exit");
-        co = null;
-    }
-
 
     public override void Clear()
     {
 
     }
-
 }
