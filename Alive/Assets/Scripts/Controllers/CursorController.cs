5591fa00922528dee49e59197b0b1fcdb76aa8f5
Joohyuck <joohyuck.seo@gmail.com>
2022-08-30 07:31:40 +0900

    마우스 커서 컨트롤러 만들기 및 적용

---
+++ Alive/Assets/Scripts/Controllers/CursorController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    Texture2D _baseIcon;
+    Texture2D _attackIcon;
+
+    public enum CursorType
+    {
+        None,
+        Base,
+        Attack,
+    }
+
+    CursorType _cursorType = CursorType.None;
+
+    int _mask = (1 << (int)Define.Layer.Ground | 1 << (int)Define.Layer.Monster);
+
+    void Start()
+    {
+        _baseIcon = Managers.Resource.Load<Texture2D>("Art/UI/Cursor/Base");
+        _attackIcon = Managers.Resource.Load<Texture2D>("Art/UI/Cursor/Attack");
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+            return;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100.0f, _mask))
+        {
+            if (hit.collider.gameObject.layer == (int)Define.Layer.Monster)
+            {
+                if (_cursorType != CursorType.Attack)
+                {
+                    Cursor.SetCursor(_attackIcon, new Vector2(_attackIcon.width * 0.4f, _attackIcon.height * 0.2f), CursorMode.Auto);
+                    _cursorType = CursorType.Attack;
+                }
+            }
+            else
+            {
+                if (_cursorType != CursorType.Base)
+                {
+                    Cursor.SetCursor(_baseIcon, new Vector2(_baseIcon.width * 0.4f, _baseIcon.height * 0.2f), CursorMode.Auto);
+                    _cursorType = CursorType.Base;
+                }
+            }
+        }
+    }
+}
--- Alive/Assets/Scripts/Scenes/BaseScene.cs
+++ Alive/Assets/Scripts/Scenes/BaseScene.cs
@@ -17,6 +17,8 @@ public abstract class BaseScene : MonoBehaviour
         Object obj = GameObject.FindObjectOfType(typeof(EventSystem));
         if (obj == null)
             Managers.Resource.Instantiate("UI/EventSystem").name = "@EventSystem";
+
+        gameObject.GetOrAddComponent<CursorController>();
     }
 
     public abstract void Clear();
