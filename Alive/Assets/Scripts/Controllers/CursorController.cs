719c4bf5ef30561318974e4455cb2f83237df34e
Joohyuck <joohyuck.seo@gmail.com>
2022-09-08 04:58:58 +0900

    Outline 몬스터에 적용

--- Alive/Assets/Scripts/Controllers/CursorController.cs
+++ Alive/Assets/Scripts/Controllers/CursorController.cs
@@ -6,6 +6,7 @@ public class CursorController : MonoBehaviour
 {
     Texture2D _baseIcon;
     Texture2D _attackIcon;
+    Outline _outline;
 
     public enum CursorType
     {
@@ -34,8 +35,14 @@ public class CursorController : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, 100.0f, _mask))
         {
+
             if (hit.collider.gameObject.layer == (int)Define.Layer.Monster)
             {
+                // Outline
+                _outline = hit.collider.gameObject.GetComponent<Outline>();
+                _outline.OutlineMode = Outline.Mode.OutlineAll;
+
+                // Attack Cursor
                 if (_cursorType != CursorType.Attack)
                 {
                     Cursor.SetCursor(_attackIcon, new Vector2(_attackIcon.width * 0.4f, _attackIcon.height * 0.2f), CursorMode.Auto);
@@ -44,6 +51,9 @@ public class CursorController : MonoBehaviour
             }
             else
             {
+                if (_outline != null)
+                    _outline.OutlineMode = Outline.Mode.OutlineHidden;
+
                 if (_cursorType != CursorType.Base)
                 {
                     Cursor.SetCursor(_baseIcon, new Vector2(_baseIcon.width * 0.4f, _baseIcon.height * 0.2f), CursorMode.Auto);
