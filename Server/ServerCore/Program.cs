9385dee5f3dd2d6cad4d4727d742879c41352667
Joohyuck <joohyuck.seo@gmail.com>
2022-09-14 16:14:40 +0900

    게임 서버 테스트 - Interlocked

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -17,51 +17,37 @@ namespace ServerCore
 
     class Program
     {
-        static int x = 0;
-        static int y = 0;
-        static int r1 = 0;
-        static int r2 = 0;
+        static int number = 0;
+        
+        // atomic = 원자성
 
         static void Thread_1()
         {
-            y = 1;      // Store y
-
-            Thread.MemoryBarrier();
-
-            r1 = x;     // Load x
+            for (int i = 0; i < 100000; i++)
+            {
+                // All or Nothing
+                int afterValue = Interlocked.Increment(ref number);
+            }
         }
 
         static void Thread_2()
         {
-            x = 1;      // Store x
-
-            Thread.MemoryBarrier();
-
-            r2 = y;     // Load y
+            for (int i = 0; i < 100000; i++)
+            {
+                Interlocked.Decrement(ref number);
+            }
         }
 
         static void Main(string[] args)
         {
-            int count = 0;
+            Task t1 = new Task(Thread_1);
+            Task t2 = new Task(Thread_2);
+            t1.Start();
+            t2.Start();
 
-            while(true)
-            {
-                count++;
-                x = y = r1 = r2 = 0;
+            Task.WaitAll(t1, t2);
 
-                Task t1 = new Task(Thread_1);
-                Task t2 = new Task(Thread_2);
-
-                t1.Start();
-                t2.Start();
-
-                Task.WaitAll(t1, t2);
-
-                if (r1 == 0 && r2 == 0)
-                    break;
-            }
-
-            Console.WriteLine($"{count}번만에 빠져나옴!");
+            Console.WriteLine(number);
         }
     }
 }
